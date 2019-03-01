66f0b14fc40d11c8b6d8b69ebd7dd88d5f772503
unknown <pubudusenanayake63@gmail.com>
2019-03-01 11:58:20 +0530

    add initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
